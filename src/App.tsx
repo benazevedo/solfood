11180d84989ecd8a2969447bd32ab96362687c9f
ben.azevedo <ben.azevedo@metergroup.com>
2024-09-07 21:42:33 -0700

    Completed home page, but images will not show

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,154 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [searchActive, setSearchActive] = useState(false);
+  const [menuActive, setMenuActive] = useState(false);
+  const [imgSrc, setImgSrc] = useState('./images/food-1.jpeg');
+  const [flipped, setFlipped] = useState(false);
+
+  const toggleSearch = () => {
+    setSearchActive(!searchActive);
+  };
+
+  const toggleMenu = () => {
+    setMenuActive(!menuActive);
+  };
+
+  const toggleFlip = () => {
+    setFlipped(!flipped);
+  };
+
+  const changeImg = (img: string) => {
+    setImgSrc(`./images/${img}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header>
+        <a href="/" className="logo">
+          <ion-icon name="restaurant-outline"></ion-icon>
         </a>
+
+        <div className={`search ${searchActive ? 'active' : ''}`}>
+          <ion-icon name="search-outline" onClick={toggleSearch}></ion-icon>
+          <input type="text" placeholder="SEARCH" />
+        </div>
+
+        <div className="bar">
+          <ul className="nav">
+            <li>
+              <a href="#">Breakfast</a>
+            </li>
+            <li>
+              <a href="#">Lunch</a>
+            </li>
+            <li>
+              <a href="#">Dinner</a>
+            </li>
+          </ul>
+          <div className="check-out">
+            <ion-icon name="bag-outline"></ion-icon>
+            <span className="number">4</span>
+          </div>
+          <a className="menu" onClick={toggleMenu}>
+            <ion-icon name="ellipsis-vertical-outline"></ion-icon>
+          </a>
+        </div>
+
+        <ul className={`nav-mobile ${menuActive ? 'active' : ''}`}>
+          <li>
+            <a href="#">Breakfast</a>
+          </li>
+          <li>
+            <a href="#">Lunch</a>
+          </li>
+          <li>
+            <a href="#">Dinner</a>
+          </li>
+        </ul>
       </header>
+
+      <div className={`banner ${flipped ? 'active' : ''}`}>
+        <div className="content">
+          <h1>
+            Sol <ion-icon name="sunny-outline"></ion-icon>
+            <br />
+          </h1>
+          <h2>Food</h2>
+        </div>
+        <div className="imgBox">
+          <div className="food">
+            <img className="food-img" src={imgSrc} alt="food" />
+          </div>
+          <div className="description">
+            <h3>Lorem ipsum dolor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <ul className="download">
+              <li>
+                <img src="./images/apple.png" alt="Apple" />
+              </li>
+              <li>
+                <img src="./images/google.png" alt="Google" />
+              </li>
+            </ul>
+            <span className="price">$37.59</span>
+          </div>
+        </div>
+        <a className="btn" href="#" onClick={toggleFlip}>
+          Learn more <ion-icon name="search-outline"></ion-icon>
+        </a>
+        <span className="order">
+          <ion-icon name="bag-add-outline"></ion-icon>
+        </span>
+        <div className="selections">
+          <div className="circle">
+            <div
+              className="options one"
+              onClick={() => changeImg('food-1.jpeg')}
+            >
+              <img src="./images/food-1.jpeg" alt="Food 1" />
+            </div>
+            <div
+              className="options two"
+              onClick={() => changeImg('food-2.jpeg')}
+            >
+              <img src="./images/food-2.jpeg" alt="Food 2" />
+            </div>
+            <div
+              className="options three"
+              onClick={() => changeImg('food-3.jpeg')}
+            >
+              <img src="./images/food-3.jpeg" alt="Food 3" />
+            </div>
+          </div>
+          <h1>Healthy Life</h1>
+        </div>
+      </div>
+
+      <ul className="sci">
+        <li>
+          <ion-icon name="logo-facebook"></ion-icon>
+        </li>
+        <li>
+          <ion-icon name="logo-twitter"></ion-icon>
+        </li>
+        <li>
+          <ion-icon name="logo-instagram"></ion-icon>
+        </li>
+      </ul>
+
+      <footer>
+        <div className="copyright">
+          &copy; Copyright{' '}
+          <strong>
+            <span>Azevedo Technologies Inc.</span>
+          </strong>{' '}
+          All Rights Reserved
+        </div>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
